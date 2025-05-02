594737d5eb326568a1088b1f06f0f617540cfb4a
Colby Stoker <b0id.lonewolf.dev@gmail.com>
2025-05-01 20:33:16 -0400

    Update footer tagline: 🦖 I am a man of many talents. (from the 80486 era)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = ({ currentTheme }) => {
             />
             <div>
               <div className="footer-logo">b0id.dev</div>
-              <div className="footer-tagline">🔄 Failure is my Fork Button — Collapse doesn’t scare me. It’s just another chance to patch and push.</div>
+              <div className="footer-tagline">🦖 I am a man of many talents. (from the 80486 era)</div>
             </div>
           </div>
         </div>
