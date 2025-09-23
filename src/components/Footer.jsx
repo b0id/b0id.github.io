31b12842cad18192420d8126c2127f1ef1714b99
Colby Stoker <b0id.lonewolf.dev@gmail.com>
2025-09-22 20:07:53 -0400

    Update footer tagline: 😎Catch you in the next merge conflict.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = ({ currentTheme }) => {
             />
             <div>
               <div className="footer-logo">b0id.dev</div>
-              <div className="footer-tagline">🧩 Problem-Solving Polyglot — From microcontrollers to mental health, if there’s a system, I can make it talk.</div>
+              <div className="footer-tagline">😎Catch you in the next merge conflict. </div>
             </div>
           </div>
         </div>
