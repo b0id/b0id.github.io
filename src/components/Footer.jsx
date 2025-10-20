4c3d2f5f86e48dce990d6b865d177eb090409166
Colby Stoker <b0id.lonewolf.dev@gmail.com>
2025-10-19 20:08:53 -0400

    Update footer tagline: 🌹Roses are red, violets are blue, intelligence is learned, can you learn too?

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = ({ currentTheme }) => {
             />
             <div>
               <div className="footer-logo">b0id.dev</div>
-              <div className="footer-tagline">🎛️ Bash-Powered Brainwaves — my scripts are philosophies. Every terminal window is a mirror of how I think.</div>
+              <div className="footer-tagline">🌹Roses are red, violets are blue, intelligence is learned, can you learn too?</div>
             </div>
           </div>
         </div>
