affbfe8693f2d7988d3cf6ac25234ead7cae72db
Colby Stoker <b0id@sdf.org>
2026-02-01 19:15:52 -0500

    Update footer tagline: 😎Catch you in the next merge conflict.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = ({ currentTheme }) => {
             />
             <div>
               <div className="footer-logo">b0id.dev</div>
-              <div className="footer-tagline">🌹Roses are red, violets are blue, intelligence is learned, can you learn too?</div>
+              <div className="footer-tagline">😎Catch you in the next merge conflict. </div>
             </div>
           </div>
         </div>
