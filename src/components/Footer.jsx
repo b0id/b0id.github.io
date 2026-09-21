9a3bbcebf2acc0924df5fe21991d06605fa32c13
Colby Stoker <b0id@sdf.org>
2026-09-20 22:23:41 -0400

    Update footer tagline: 🦖 I am a man of many talents. (from the 80486 era)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = ({ currentTheme }) => {
             />
             <div>
               <div className="footer-logo">b0id.dev</div>
-              <div className="footer-tagline">🌱 Fractal Integrity — I act the same way in my codebase, my care plans, and my convictions.</div>
+              <div className="footer-tagline">🦖 I am a man of many talents. (from the 80486 era)</div>
             </div>
           </div>
         </div>
